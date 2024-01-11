64006378949484f20c00efc75e968d560a3c8b14
Pablo Javier Iglesias Castañón <93825670+pabiglesiasc@users.noreply.github.com>
2024-01-11 18:56:28 +0000

    -

--- modules/dataretriever.py
+++ modules/dataretriever.py
@@ -4,9 +4,5 @@ import numpy as np
 def get_yfinance():
     import yfinance as yf
 
-
-
-
-
 def get_data(start_date, end_date, stocks, macroeconomic, correlated, extra_stocks, macroeconomic_indicators, number_correlated):
     return None
